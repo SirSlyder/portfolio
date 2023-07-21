25d1c201057d34ee727fe2e7ccbb54abf5a8eb3d
leifkemp-bjss <leif.kemp@bjss.com>
2023-07-21 02:23:04 +0100

    Revert "Deploying to master from @ SirSlyder/portfolio@949818720df0f2f1e09040e5eae418200e802fad 🚀"
    This reverts commit 41ffcc9b9f22ee68bc8325938e9f1be3a7721825.

---
+++ src/app/games/buddy/buddy.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuddyComponent } from './buddy.component';
+
+describe('BuddyComponent', () => {
+  let component: BuddyComponent;
+  let fixture: ComponentFixture<BuddyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuddyComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BuddyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
